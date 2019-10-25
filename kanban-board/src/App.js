0db2ca55f39cb81a64a390896885fb75bce30388
macandrew6 <mr.betterandrewmac@gmail.com>
2019-10-25 15:21:31 -0700

    creating kanban divs

--- kanban-board/src/App.js
+++ kanban-board/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>hello world</h2>
+        <h2>This will be a kanban board</h2>
       </div>
     );
   }
