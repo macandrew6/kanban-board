3378f3f344509b9fe19d8e050208a943552b1137
macandrew6 <mr.betterandrewmac@gmail.com>
2019-10-25 15:24:55 -0700

    init Todo Board

--- kanban-board/src/App.js
+++ kanban-board/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import TodoBoard from './components/TodoBoard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>This will be a kanban board</h2>
+        <h2>Kanban board</h2>
+        <TodoBoard />
       </div>
     );
   }
---
+++ kanban-board/src/components/TodoBoard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TodoBoard extends Component {
+  render() {
+    return (
+      <div>
+        <h2>hello from TodoBoard</h2>
+      </div>
+    );
+  }
+}
