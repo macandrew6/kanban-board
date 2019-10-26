e677a9aa30996754a83d347aa4b27917bdee04c8
macandrew6 <mr.betterandrewmac@gmail.com>
2019-10-26 12:34:44 -0700

    makes state in todoboard

--- kanban-board/src/components/TodoBoard.js
+++ kanban-board/src/components/TodoBoard.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 export default class TodoBoard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+  
   render() {
     return (
       <div className="todoboard-container board">
         <h3>hello from TodoBoard</h3>
+        <div>
+        
+        </div>
       </div>
     );
   }
