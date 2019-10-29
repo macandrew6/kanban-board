8dc4b1fc512d393bd8f18a50686d23a6153f2e19
macandrew6 <mr.betterandrewmac@gmail.com>
2019-10-28 17:01:08 -0700

    successful renders of all lists

--- kanban-board/src/components/CompletedBoard.js
+++ kanban-board/src/components/CompletedBoard.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 export default class CompletedBoard extends Component {
   render() {
+    const { completedList } = this.props;
+
     return (
       <div className="completedboard-container board">
         <h3>CompletedBoard</h3>
+        <div>
+          {completedList.map((completed, i) => (
+            <div>{completed}</div>
+          ))}
+        </div>
       </div>
     );
   }
--- kanban-board/src/components/InprogressBoard.js
+++ kanban-board/src/components/InprogressBoard.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 export default class InprogressBoard extends Component {
   render() {
+    const { inProgressList } = this.props;
+
     return (
       <div className="inprogressboard-container board">
         <h3>InprogressBoard</h3>
+        <div>
+          {inProgressList.map((inProgress, i) => (
+            <div>{inProgress}</div>
+          ))}
+        </div>
       </div>
     );
   }
