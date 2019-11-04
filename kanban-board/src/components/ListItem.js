2d6b5e3e3f92a7ad980c1c35e022251d9e137b88
macandrew6 <mr.betterandrewmac@gmail.com>
2019-11-04 15:43:36 -0800

    starts delete and edit fn

--- kanban-board/src/components/ListItem.js
+++ kanban-board/src/components/ListItem.js
@@ -11,13 +11,14 @@ export default class ListItem extends Component {
   
   render() {
     const { todo, moveTodoLeft, moveTodoRight } = this.props;
-    console.log(todo);
     return (
       <div>
         {todo.text}
         <div>
           <button onClick={(e) => moveTodoLeft(e, todo.id)} value="left">Left</button>
           <button onClick={(e) => moveTodoRight(e, todo.id)} value="right">Right</button>
+          <button>Delete</button>
+          <button>Edit</button>
         </div>
       </div>
     );
