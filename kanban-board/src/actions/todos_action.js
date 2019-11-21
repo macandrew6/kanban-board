d29e775ae5fb09efe5c16a5eeee6a0e902981491
macandrew6 <mr.betterandrewmac@gmail.com>
2019-11-20 19:39:17 -0800

    adds action files

---
+++ kanban-board/src/actions/todos_action.js
@@ -0,0 +1,13 @@
+export const RECEIVE_ALL_TODOS = 'RECEIVE_ALL_TODOS';
+export const RECEIVE_TODO = "RECEIVE_TODO";
+export const REMOVE_TODO = "REMOVE_TODO";
+
+const receiveAllTodos = todos => ({
+  type: RECEIVE_ALL_TODOS,
+  todos
+});
+
+const receiveTodo = todo => ({
+  type: RECEIVE_TODO,
+  todo
+});
--- kanban-board/src/reducers/root_reducer.js
+++ kanban-board/src/reducers/root_reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import todoReducer from 'todo_reducer.js';
+import todosReducer from './todos_reducer.js';
 
 export default combineReducers({
   todos: todosReducer
