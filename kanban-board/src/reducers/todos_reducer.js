f4ea2ced4fa61a4cf1aa04cec33402565711cba3
macandrew6 <mr.betterandrewmac@gmail.com>
2019-11-26 00:35:22 -0800

    working on reducers

--- kanban-board/src/reducers/todos_reducer.js
+++ kanban-board/src/reducers/todos_reducer.js
@@ -4,3 +4,14 @@ import {
   REMOVE_TODO
 } from '../actions/todos_action';
 
+export default (oldState = {}, action) => {
+  Object.freeze(oldState);
+  switch (action.type) {
+    case RECEIVE_ALL_TODOS:
+      return Object.assign({}, oldState, action.todos);
+    case RECEIVE_TODO:
+      return Object.assign({}, oldState, {[action.todo.id]: action.todo});
+    default:
+      return oldState;
+  }
+}
