fb2ca64e516e76ccf3ba981ae1de6fa2a5b08caa
macandrew6 <mr.betterandrewmac@gmail.com>
2019-12-09 15:04:54 -0800

    fixes container

--- kanban-board/src/AppContainer.js
+++ kanban-board/src/AppContainer.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import App from './App.js';
 
 // mapStateToProps
-const mapStateToProps = ({state, ownProps}) => {
+const mapStateToProps = (state) => {
   return {
     allListItems: state.todos
   };
