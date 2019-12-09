80d2769edb1ec67177e26291a5f9f35517119a18
macandrew6 <mr.betterandrewmac@gmail.com>
2019-12-09 13:36:22 -0800

    finishing off app container

--- kanban-board/src/AppContainer.js
+++ kanban-board/src/AppContainer.js
@@ -1,9 +1,21 @@
 // connect to redux store
 import {connect} from 'redux';
 // import App from "./App.js";
+import App from './App.js';
 
 // mapStateToProps
+const mapStateToProps = ({state, ownProps}) => {
+
+  return {
+
+  };
+};
 
 // mapDispatchToProps
+const mapDispatchToProps = ({dispatch}) => {
+  return {
 
-// connect(mapStateToProps, mapDispatchToProps)(App)
+  };
+};
+
+connect(mapStateToProps, mapDispatchToProps)(App);
