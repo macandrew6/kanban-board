cefaf8c08c4caea84e86886d25f222e1256de311
macandrew6 <mr.betterandrewmac@gmail.com>
2019-12-11 15:46:19 -0800

    adds localStorage file

--- kanban-board/src/index.js
+++ kanban-board/src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './root';
 import './index.css';
-import configureStore from './store';
 
-let store = configureStore();
 
 ReactDOM.render(
-  <Root store={store}/>,
+  <Root />,
   document.getElementById('root')
 );
--- kanban-board/src/root.js
+++ kanban-board/src/root.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import AppContainer from "./AppContainer";
+import configureStore from './store';
 import { Provider } from "react-redux";
+import { loadState } from './localStorage';
 
-const Root = ({ store }) => {
+const persistedState = loadState();
+const store = configureStore(persistedState);
+
+const Root = () => {
   return (
     <div>
       <Provider store={store}>
