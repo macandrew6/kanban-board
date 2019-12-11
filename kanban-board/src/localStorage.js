afb0b4a13336d24cc73fab3d1b32dc8210057e29
macandrew6 <mr.betterandrewmac@gmail.com>
2019-12-11 15:53:19 -0800

    writing try catch function

--- kanban-board/src/localStorage.js
+++ kanban-board/src/localStorage.js
@@ -1,3 +1,11 @@
 export const loadState = () => {
-  
+  try {
+    const serializeState = localStorage.getItem('state');
+    if (serializeState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializeState);
+  } catch (err) {
+    return undefined;
+  }
 };
